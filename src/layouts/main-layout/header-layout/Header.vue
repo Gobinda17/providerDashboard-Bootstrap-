34c98e53a9530fcabcc1ae5fe34c18b3d3c1e038
Gobinda17 <gobindadeb14@gmail.com>
2024-07-09 17:53:18 +0530

    Dashboard

--- src/layouts/main-layout/header-layout/Header.vue
+++ src/layouts/main-layout/header-layout/Header.vue
@@ -4,7 +4,7 @@
         <!--begin::Header container-->
         <div class="container-fluid app-container d-flex align-items-stretch justify-content-between">
             <!--begin::sidebar mobile toggle-->
-            <div class="d-flex align-items-center d-lg-none ms-n3 me-1 me-md-2" v-tooltip title="Show sidebar menu">
+            <div class="d-flex align-items-center d-lg-none ms-n3 me-1 me-md-2" v-tooltip title="Show sidebar menu" @click="showMobileMenu()">
                 <div class="btn btn-icon btn-active-color-primary w-35px h-35px" id="mz_app_sidebar_mobile_toggle">
                     <i class="ri-menu-2-line" style="font-size: 2rem;"></i>
                 </div>
@@ -27,5 +27,15 @@ export default {
     components: {
         HeaderNav,
     },
+    methods: {
+        showMobileMenu() {
+            let menuShow = document.getElementById('mz_app_sidebar').attributes['data-mz-drawer'].value;
+            if(menuShow == "true") {
+                document.getElementById('mz_app_sidebar').attributes['data-mz-drawer'].value = "false";
+            } else {
+                document.getElementById('mz_app_sidebar').attributes['data-mz-drawer'].value = "true";
+            }
+        }
+    }
 }
 </script>
