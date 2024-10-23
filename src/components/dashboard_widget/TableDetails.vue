7de1f09f593e8d33096f3698f77944bbf969ac8a
Gobinda17 <gobindadeb14@gmail.com>
2024-10-23 22:38:45 +0530

    Adventure added

--- src/components/dashboard_widget/TableDetails.vue
+++ src/components/dashboard_widget/TableDetails.vue
@@ -28,7 +28,80 @@
                     role="tabpanel">
                     <div class="mb-5">
                         <div class="table-responsive">
-                            <table id="dataTableWeek" class="table table-row-dashed align-middle gs-0 gy-3 my-0">
+                            <DataTable width="100%" ajax="bookings" id="dataTableWeek" class="table table-row-dashed align-middle gs-0 gy-3 my-0">
+                                <thead>
+                                    <tr class="fs-7 fw-bold text-gray-400 border-bottom-0">
+                                        <th class="p-0 pb-3 min-w-80px text-start">Booking ID</th>
+                                        <th class="p-0 pb-3 min-w-150px text-end">Customer Name</th>
+                                        <th class="p-0 pb-3 min-w-150px text-end">Package/Tour Name</th>
+                                        <th class="p-0 pb-3 min-w-100px text-end">Tour/Activity Date</th>
+                                        <th class="p-0 pb-3 min-w-100px text-end">Number of Participants</th>
+                                        <th class="p-0 pb-3 min-w-175px text-end pe-12">Total Price</th>
+                                        <th class="p-0 pb-3 min-w-175px text-end pe-12">Payment Status</th>
+                                        <th class="p-0 pb-3 min-w-175px text-end pe-12">Booking Status</th>
+                                        <th class="p-0 pb-3 min-w-175px text-end pe-12">Contact Information</th>
+                                        <th class="p-0 pb-3 w-50px text-end">VIEW</th>
+                                    </tr>
+                                </thead>
+                                <!-- <tbody>
+                                    <template v-for="(row, index) in bookings" :key="index">
+                                        <tr>
+                                            <td>
+                                                <span class="text-gray-600 fw-bold fs-6">{{ row.id }}</span>
+                                            </td>
+                                            <td class="text-end pe-0">
+                                                <span class="text-gray-600 fw-bold fs-6">{{ row.name }}</span>
+                                            </td>
+                                            <td class="text-end pe-0">
+                                                <span class="text-gray-600 fw-bold fs-6">{{ row.tour }}</span>
+                                            </td>
+                                            <td class="text-end pe-0">
+                                                <span class="text-gray-600 fw-bold fs-6">{{ row.t_date }}</span>
+                                            </td>
+                                            <td class="text-end pe-0">
+                                                <span class="text-gray-600 fw-bold fs-6">{{ row.b_date }}</span>
+                                            </td>
+                                            <td class="text-end pe-0">
+                                                <span class="text-gray-600 fw-bold fs-6">{{ row.capacity }}</span>
+                                            </td>
+                                            <td class="text-end pe-0">
+                                                <span class="text-gray-600 fw-bold fs-6">{{ row.t_price }}</span>
+                                            </td>
+                                            <td class="text-end pe-0">
+                                                <span class="text-gray-600 fw-bold fs-6 badge text-bg-success">
+                                                    {{ row.p_status }}
+                                                </span>
+                                            </td>
+                                            <td class="text-end pe-0">
+                                                <span class="text-gray-600 fw-bold fs-6 badge text-bg-danger">
+                                                    {{ row.b_status }}
+                                                </span>
+                                            </td>
+                                            <td class="text-end pe-0">
+                                                <span class="text-gray-600 fw-bold fs-6">
+                                                    {{ row.contact }}
+                                                </span>
+                                            </td>
+                                            <td class="text-end pe-0">
+                                                <span class="text-dark-600 fw-bold fs-6">
+                                                    <a href="#"
+                                                        class="btn btn-sm btn-icon btn-bg-dark btn-active-color-primary w-30px h-30px">
+                                                        View
+                                                    </a>
+                                                </span>
+                                            </td>
+                                        </tr>
+                                    </template>
+                                </tbody> -->
+                            </DataTable>
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-pane fade" :class="view === 'month' ? ' active show' : ''" id="#month_tab"
+                    role="tabpanel">
+                    <div class="mb-5">
+                        <div class="table-responsive">
+                            <table id="dataTableMonth" class="table table-row-dashed align-middle gs-0 gy-3 my-0">
                                 <thead>
                                     <tr class="fs-7 fw-bold text-gray-400 border-bottom-0">
                                         <th class="p-0 pb-3 min-w-80px text-start">Booking ID</th>
@@ -97,17 +170,16 @@
                         </div>
                     </div>
                 </div>
-                <div class="tab-pane fade" :class="view === 'month' ? ' active show' : ''" id="#month_tab"
-                    role="tabpanel">
-                    <div class="mb-5">
-
-                    </div>
-                </div>
             </div>
         </div>
     </div>
 </template>
 <script>
+import DataTable from 'datatables.net-vue3';
+import DataTablesCore from 'datatables.net-bs5';
+
+DataTable.use(DataTablesCore);
+
 export default {
     name: 'TableDetails',
     data() {
@@ -132,14 +204,14 @@ export default {
             this.view = value;
         },
         fetchData() {
-            $("#dataTableWeek").DataTable({
-                pageLength: 10,
-                ordering: false,
-                lengthChange: false,
-                searching: true,
-                info: false,
-                bDestroy: true
-            });
+            // $("#dataTableWeek").DataTable({
+            //     pageLength: 10,
+            //     ordering: false,
+            //     lengthChange: false,
+            //     searching: true,
+            //     info: false,
+            //     bDestroy: true
+            // });
         }
     },
     mounted() {
@@ -147,3 +219,7 @@ export default {
     }
 }
 </script>
+<style scoped>
+@import 'bootstrap';
+@import 'datatables.net-bs5';
+</style>
